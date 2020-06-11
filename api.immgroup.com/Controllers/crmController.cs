825ee74edeb5a3ed218f7c52813f0b537a78e010
svit90 <56331742+svit90@users.noreply.github.com>
2020-06-11 15:03:43 +0700

    first blood

---
+++ api.immgroup.com/Controllers/crmController.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using api.immgroup.com;
+
+namespace api.immgroup.com.Controllers
+{
+    [ApiController]
+    public class crmController : ControllerBase
+    {
+        [Produces("application/json")]
+        [Route("crm/")]
+        [ProducesResponseType(200, Type = typeof(JsonResult))]
+        [AllowAnonymous]
+        public string default_string()
+        {
+            try
+            {
+                return "Welcome To IMM GROUP API";
+            }
+            catch (Exception)
+            {
+                return "Welcome To IMM GROUP API";
+            }
+        }
+
+        [Produces("application/json")]
+        [Route("crm/login/{email}")]
+        [ProducesResponseType(200, Type = typeof(JsonResult))]
+        [AllowAnonymous]
+        public IActionResult GetStaff(string email)
+        {
+            try
+            {
+                using (var db = new SqlConnection(DBHelper.connectionString))
+                {
+                    const string sql = "[dbo].[wb_FUNCTION_Login_Oop_define]";
+
+                    var items = db.Query<dynamic>(sql: sql, param: new { @Email = email }, commandType: CommandType.StoredProcedure).ToList();
+
+                    var response = new
+                    {
+                        ok = true,
+                        customers = items,
+                    };
+
+                    return new OkObjectResult(response);
+                }
+            }
+            catch (Exception e)
+            {
+                var response = new
+                {
+                    ok = false,
+                    error = e.Message
+                };
+
+                return new BadRequestObjectResult(response);
+            }
+        }
+
+
+        [Produces("application/json")]
+        [Route("crm/message/{lang}/{code}")]
+        [ProducesResponseType(200, Type = typeof(JsonResult))]
+        [AllowAnonymous]
+        public IActionResult GetMessageByCode(string lang,string code)
+        {
+            try
+            {
+                var response = new
+                {
+                    ok = true,
+                    mess = Error.Show(lang, code),
+                };
+
+                return new OkObjectResult(response);
+            }
+            catch (Exception e)
+            {
+                var response = new
+                {
+                    ok = false,
+                    error = e.Message
+                };
+
+                return new BadRequestObjectResult(response);
+            }
+        }
+
+
+        /*FUNCTION API FOR USER START*/
+        [Produces("application/json")]
+        [Route("crm/search/{key}")]
+        [ProducesResponseType(200, Type = typeof(JsonResult))]
+        [AllowAnonymous]
+        public IActionResult GetInfoUserById(string key)
+        {
+            try
+            {
+                
+                using (var db = new SqlConnection(DBHelper.connectionString))
+                {
+                    const string sql = "[dbo].[_012020_CRM_V3_FUNC_Search_All_Cus]";
+
+                    var items = db.Query<dynamic>(sql: sql, param: new { @Key = key }, commandType: CommandType.StoredProcedure).ToList();
+
+                    var response = new
+                    {
+                        ok = true,
+                        customers = items,
+                    };
+
+                    return new OkObjectResult(response);
+                }
+            }
+            catch (Exception e)
+            {
+                var response = new
+                {
+                    ok = false,
+                    error = e.Message
+                };
+
+                return new BadRequestObjectResult(response);
+            }
+        }
+        /*FUNCTION API FOR USER END*/
+
+        /*FUNCTION API FOR MENU START*/
+        [Produces("application/json")]
+        [Route("crm/menu/all")]
+        [ProducesResponseType(200, Type = typeof(JsonResult))]
+        [AllowAnonymous]
+        public IActionResult GetAllMenu()
+        {
+            try
+            {
+
+                using (var db = new SqlConnection(DBHelper.connectionString))
+                {
+                    const string sql = "[dbo].[_012020_CRM_V3_api_get_All_Menu]";
+                    var items = db.Query<dynamic>(sql: sql, commandType: CommandType.StoredProcedure).ToList();
+                    return new OkObjectResult(items);
+                }
+            }
+            catch (Exception e)
+            {
+                var response = new
+                {
+                    ok = false,
+                    error = e.Message
+                };
+
+                return new BadRequestObjectResult(response);
+            }
+        }
+        [Produces("application/json")]
+        [Route("crm/menu")]
+        [ProducesResponseType(200, Type = typeof(JsonResult))]
+        [AllowAnonymous]
+        public IActionResult GetMenu()
+        {
+            try
+            {
+
+                using (var db = new SqlConnection(DBHelper.connectionString))
+                {
+                    const string sql = "[dbo].[_012020_CRM_V3_api_get_All_Menu]";
+                    var items = db.Query<dynamic>(sql: sql, commandType: CommandType.StoredProcedure).ToList();
+                    var response = new
+                    {
+                        menu = items
+                    };
+                    return new OkObjectResult(response);
+                }
+            }
+            catch (Exception e)
+            {
+                var response = new
+                {
+                    ok = false,
+                    error = e.Message
+                };
+
+                return new BadRequestObjectResult(response);
+            }
+        }
+        /*FUNCTION API FOR MENU END*/
+    }
+}
---
+++ api.immgroup.com/Models/ClassUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.immgroup.com.Models
+{
+    public class ClassUser
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPhone { get; set; }
+
+        
+    }
+}
